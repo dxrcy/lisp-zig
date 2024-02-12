bca20a32b922baf959a9d1980cc5664aae4441b5
darcy <acednes@gmail.com>
2024-02-13 10:19:21 +1100

    parse token tree

--- lisp.zig
+++ lisp.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const fs = std.fs;
 const print = std.debug.print;
+const panic = std.debug.panic;
 const ArrayList = std.ArrayList;
 
 pub fn main() !void {
@@ -12,7 +13,7 @@ pub fn main() !void {
     const file = try fs.cwd().openFile(filename, .{});
     defer file.close();
 
-    const tokens = try lex_tokens(&file, allocator);
+    var tokens = try lex_tokens(&file, allocator);
     defer tokens.deinit();
     defer for (tokens.items) |token| {
         token.deinit();
@@ -22,16 +23,79 @@ pub fn main() !void {
         print("Token: {}\n", .{token});
     }
 
-    const tree = try parse_tree(tokens, allocator);
+    print("\n", .{});
+    var index: usize = 0;
+    const tree = try parse_tree(&tokens, &index, 0, allocator);
     defer tree.deinit();
-    print("\nTree: {any}\n", .{tree});
+    print("\n", .{});
+    tree.debug_print(0);
 }
 
-fn parse_tree(tokens: ArrayList(Token), allocator: anytype) !TokenTree {
-    _ = tokens;
-    var string = ArrayList(u8).init(allocator);
-    try string.appendSlice("bruh");
-    return TokenTree{ .literal = string };
+fn parse_tree(tokens: *ArrayList(Token), index: *usize, depth: usize, allocator: anytype) !TokenTree {
+    if (depth > 20) {
+        panic("max recursion for parse_tree", .{});
+    }
+
+    if (tokens.items.len == 0) {
+        return error.NoTokens;
+    }
+    if (tokens.items.len == 1) {
+        const token = tokens.items[0];
+        switch (token) {
+            .literal => |lit| {
+                return TokenTree{ .literal = lit };
+            },
+            else => return error.SingleBracket,
+        }
+    }
+
+    var group = ArrayList(TokenTree).init(allocator);
+
+    while (index.* < tokens.items.len) {
+        const token = tokens.items[index.*];
+        index.* += 1;
+
+        print("{?}\n", .{token});
+        switch (token) {
+            .literal => |lit| {
+                var literal = ArrayList(u8).init(allocator);
+                try literal.appendSlice(lit.items);
+                try group.append(TokenTree{ .literal = literal });
+            },
+            .br_left => {
+                const subgroup = try parse_tree(tokens, index, depth + 1, allocator);
+                try group.append(subgroup);
+            },
+            .br_right => {
+                if (depth == 0) {
+                    return error.UnexpectedRightBracket;
+                }
+                return TokenTree{ .group = group };
+            },
+        }
+    }
+
+    if (depth > 0) {
+        return error.UnexpectedEndOfStream;
+    }
+
+    return TokenTree{ .group = group };
+}
+
+// var a = ArrayList(TokenTree).init(allocator);
+// try a.append(TokenTree{ .literal = try string("*", allocator) });
+// var b = ArrayList(TokenTree).init(allocator);
+// try b.append(TokenTree{ .literal = try string("+", allocator) });
+// try b.append(TokenTree{ .literal = try string("12", allocator) });
+// try b.append(TokenTree{ .literal = try string("3", allocator) });
+// try a.append(TokenTree{ .group = b });
+// try a.append(TokenTree{ .literal = try string("81", allocator) });
+// return TokenTree{ .group = a };
+
+fn string(comptime literal: []const u8, allocator: anytype) !ArrayList(u8) {
+    var array = ArrayList(u8).init(allocator);
+    try array.appendSlice(literal);
+    return array;
 }
 
 const TokenTree = union(enum) {
@@ -47,6 +111,31 @@ const TokenTree = union(enum) {
                 array.deinit();
             },
             .literal => |lit| lit.deinit(),
+        }
+    }
+
+    fn debug_print(self: TokenTree, comptime depth: usize) void {
+        if (depth > 20) {
+            panic("max recursion for debug_print", .{});
+        }
+        switch (self) {
+            .group => |array| {
+                print("{s}", .{"    " ** depth});
+                print("\x1b[33m", .{});
+                print("(\n", .{});
+                print("\x1b[0m", .{});
+                for (array.items) |item| {
+                    debug_print(item, depth + 1);
+                }
+                print("{s}", .{"    " ** depth});
+                print("\x1b[33m", .{});
+                print(")\n", .{});
+                print("\x1b[0m", .{});
+            },
+            .literal => |lit| {
+                print("{s}", .{"    " ** depth});
+                print("{s}\n", .{lit.items});
+            },
         }
     }
 };
